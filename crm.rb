d4f6b46fdcd034b98c98dfdafc91acd45ecad5c2
beewhoo <bibeksinghthapa@gmail.com>
2018-06-11 22:51:45 -0400

    completed

--- crm.rb
+++ crm.rb
@@ -6,14 +6,19 @@ get '/' do
   erb :home
 end
 
+
 get '/home' do
-  erb :home
+  erb :new
 end
 
 get '/contacts' do
   @contacts = Contact.all
   @count = Contact.count
   erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new
 end
 
 get '/about' do
@@ -24,6 +29,17 @@ get '/layout' do
   erb :layout
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+  erb :edit_contact
+    else
+   raise Sinatra::NotFound
+  end
+  erb :edit_contact
+end
+
+
 get '/contacts/:id' do
   params[:id]
   @contact = Contact.find_by({id: params[:id].to_i})
@@ -33,6 +49,44 @@ get '/contacts/:id' do
    raise Sinatra::NotFound
   end
 end
+
+post '/contacts' do
+  Contact.create(
+     first_name: params[:first_name],
+     last_name:  params[:last_name],
+     email:      params[:email],
+     note:       params[:note]
+   )
+   redirect to ('/contacts')
+
+ end
+
+ put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 
 
 after do
